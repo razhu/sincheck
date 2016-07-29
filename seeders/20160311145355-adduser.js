6c8a90bb3b2049b614f6a6de2484d26e95ba217e
Ramiro <rhuayller@agetic.gob.bo>
2016-07-29 09:07:18 -0400

    se agrego datos falso

--- seeders/20160311145355-adduser.js
+++ seeders/20160311145355-adduser.js
@@ -1,34 +1,57 @@
 'use strict';
 module.exports = {
-  up: function (queryInterface, Sequelize) {
+    up: function(queryInterface, Sequelize) {
         var fecha = new Date();
-        var usuario = 'admin';
-        var contrasena = 'admin'
-      return queryInterface.bulkInsert('usuarios', [{
-        usuario: usuario,
-        contrasena: contrasena,
-        nit: 123,
-        createdAt: fecha,
-        updatedAt: fecha
-      },
-      {
-        usuario: usuario,
-        contrasena: contrasena,
-        nit: 123,
-        createdAt: fecha,
-        updatedAt: fecha
-      }
-      
-      ], {});
-  },
+        return queryInterface.bulkInsert('usuarios', [
 
-  down: function (queryInterface, Sequelize) {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
+            {
+                nit: 12345678,
+                usuario: "rhuayler",
+                contrasena: "1234",
+                createdAt: fecha,
+                updatedAt: fecha
+            },
 
-      Example:
-      return queryInterface.bulkDelete('Person', null, {});
-    */
-  }
+            {
+                nit: 123456,
+                usuario: "gvargas",
+                contrasena: "1234",
+                createdAt: fecha,
+                updatedAt: fecha
+            },
+            {
+                nit: 154623,
+                usuario: "fhuayta",
+                contrasena: "1123",
+                createdAt: fecha,
+                updatedAt: fecha
+            }
+            ,
+            {
+                nit: 569357,
+                usuario: "rcoarite",
+                contrasena: "123",
+                createdAt: fecha,
+                updatedAt: fecha
+            },
+            {
+                nit: 335454,
+                usuario: "ejimenez",
+                contrasena: "798989",
+                createdAt: fecha,
+                updatedAt: fecha
+            }
+
+        ], {});
+    },
+
+    down: function(queryInterface, Sequelize) {
+        /*
+          Add reverting commands here.
+          Return a promise to correctly handle asynchronicity.
+    
+          Example:
+          return queryInterface.bulkDelete('Person', null, {});
+        */
+    }
 };
